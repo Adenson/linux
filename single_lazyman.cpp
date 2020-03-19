77fce165fd062557c4ef4c70422c09feb64f2b3d
Adenson <15594522896@163.com>
2020-03-19 11:24:05 +0800

    单例模式---懒汉模式

---
+++ single_lazyman.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+template <class T>
+class single
+{
+  private:
+    static T data;
+  public:
+   static T* GetInstance()
+    {
+      return &data;
+    }
+};
+template <class T>
+T single<T>::data = 123;
+int main()
+{
+  single<int> A,B;
+  cout << *A.GetInstance() << " " << A.GetInstance() << endl;
+  cout << *B.GetInstance() << " " << B.GetInstance() << endl;
+  return 0;
+}
